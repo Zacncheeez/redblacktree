746688e5c3e7e8184d49a86e9811f7ff64bf0b62
Zacncheeez <zmt.tobin@gmail.com>
2026-05-27 21:11:18 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,11 +16,11 @@ class RBTree {
 private:
     Node* root;
 
+    // ---------------- ROTATIONS ----------------
     void rotateLeft(Node* x) {
         Node* y = x->right;
-        if (!y) return;
-
         x->right = y->left;
+
         if (y->left)
             y->left->parent = x;
 
@@ -39,9 +39,8 @@ private:
 
     void rotateRight(Node* x) {
         Node* y = x->left;
-        if (!y) return;
-
         x->left = y->right;
+
         if (y->right)
             y->right->parent = x;
 
@@ -58,20 +57,78 @@ private:
         x->parent = y;
     }
 
+    // ---------------- FIX INSERT ----------------
     void fixInsert(Node* z) {
-        cout << "FixInsert called on key " << z->key << endl;
-        root->color = BLACK; // temporary placeholder
+        while (z->parent && z->parent->color == RED) {
+            Node* gp = z->parent->parent;
+
+            if (z->parent == gp->left) {
+                Node* uncle = gp->right;
+
+                // Case 1: Uncle is RED
+                if (uncle && uncle->color == RED) {
+                    z->parent->color = BLACK;
+                    uncle->color = BLACK;
+                    gp->color = RED;
+                    z = gp;
+                }
+                else {
+                    // Case 2: z is right child
+                    if (z == z->parent->right) {
+                        z = z->parent;
+                        rotateLeft(z);
+                    }
+                    // Case 3: z is left child
+                    z->parent->color = BLACK;
+                    gp->color = RED;
+                    rotateRight(gp);
+                }
+            }
+            else {
+                Node* uncle = gp->left;
+
+                if (uncle && uncle->color == RED) {
+                    z->parent->color = BLACK;
+                    uncle->color = BLACK;
+                    gp->color = RED;
+                    z = gp;
+                }
+                else {
+                    if (z == z->parent->left) {
+                        z = z->parent;
+                        rotateRight(z);
+                    }
+                    z->parent->color = BLACK;
+                    gp->color = RED;
+                    rotateLeft(gp);
+                }
+            }
+        }
+
+        root->color = BLACK;
+    }
+
+    // ---------------- PRINT TREE ----------------
+    void printHelper(Node* node, int indent) {
+        if (!node) return;
+
+        indent += 6;
+
+        printHelper(node->right, indent);
+
+        cout << endl;
+        for (int i = 6; i < indent; i++)
+            cout << " ";
+        cout << node->key << (node->color == RED ? "(R)" : "(B)") << endl;
+
+        printHelper(node->left, indent);
     }
 
 public:
     RBTree() : root(nullptr) {}
 
+    // ---------------- INSERT ----------------
     void insert(int key) {
-        if (key < 0 || key > 999) {
-            cout << "Key out of range" << endl;
-            return;
-        }
-
         Node* z = new Node(key);
         Node* y = nullptr;
         Node* x = root;
@@ -96,8 +153,11 @@ public:
         fixInsert(z);
     }
 
+    // ---------------- PUBLIC PRINT ----------------
     void print() {
-        cout << "print" << endl;
+        cout << "\n===== TREE =====\n";
+        printHelper(root, 0);
+        cout << "================\n";
     }
 };
 
@@ -107,6 +167,10 @@ int main() {
     tree.insert(10);
     tree.insert(20);
     tree.insert(15);
+    tree.insert(5);
+    tree.insert(1);
+    tree.insert(25);
+    tree.insert(2);
 
     tree.print();
 
